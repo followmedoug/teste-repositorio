577d058a348edbf9e26c22954f71c03b8ba3901d
followmedoug <douglaspmateus@gmail.com>
2020-10-12 15:39:00 -0300

    first commit

---
+++ src/components/Button/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+  height: 2rem;
+  width: 315px;
+  color: #ffffff;
+  background-color: red;
+  border: "1px solid black";
+  border-radius: 8px;
+`;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const Button = require("./components/Button");
+
+module.exports = {
+  all: Button,
+};
